d5f19b130a3b7c4091275f26de057359d10624d7
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-09-27 14:02:18 -1000

    fix: add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,26 @@
 <script>
 	import '../app.css';
 
+	import { page } from '$app/stores';
+
 	import Header from '$lib/components/Header.svelte';
 
 	export let data;
 </script>
 
+<svelte:head>
+	<title>{$page.data.title}</title>
+	{#if $page.data.meta_description}
+		<meta name="description" content={$page.data.meta_description} />
+	{/if}
+	{#if $page.data.meta_title}
+		<meta name="og:title" content={$page.data.meta_title} />
+	{/if}
+	{#if $page.data.meta_image}
+		<meta name="og:image" content={$page.data.meta_image.url} />
+		<meta name="twitter:card" content="summary_large_image" />
+	{/if}
+</svelte:head>
 <div class="text-slate-800">
 	<Header navigation={data.navigation} settings={data.settings} />
 	<main><slot /></main>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,3 +1,5 @@
+import { asText } from '@prismicio/client';
+
 import { createClient } from '$lib/prismicio';
 
 export const prerender = true;
@@ -8,6 +10,10 @@ export async function load({ fetch }) {
 	const page = await client.getByUID('page', 'home');
 
 	return {
-		page
+		page,
+		title: asText(page.data.title),
+		meta_description: page.data.meta_description,
+		meta_title: page.data.meta_title,
+		meta_image: page.data.meta_image.url
 	};
 }
