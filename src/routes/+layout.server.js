63c093bb1569d97101776f7af43f9a70ef479be8
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-10-24 09:52:53 -1000

    feat: support previews (#2)
    * feat: support previews
    
    * fix: remove unnecessary param

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,9 @@
 import { createClient } from '$lib/prismicio';
 
-export async function load() {
-	const client = createClient();
+export const prerender = 'auto';
+
+export async function load({ fetch, cookies }) {
+	const client = createClient({ fetch, cookies });
 
 	const settings = await client.getSingle('settings');
 	const navigation = await client.getSingle('navigation');
