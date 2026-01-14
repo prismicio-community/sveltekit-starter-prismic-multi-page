35142cb365a335f896a873c0109528cfaad868a6
lihbr <lihbr@users.noreply.github.com>
2026-01-14 11:02:46 +0900

    chore(deps): maintain dependencies

--- src/routes/slice-simulator/+page.svelte
+++ src/routes/slice-simulator/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { SliceSimulator } from '@slicemachine/adapter-sveltekit/simulator';
-	import { SliceZone } from '@prismicio/svelte';
+	import { SliceSimulator, SliceZone } from '@prismicio/svelte';
 	import { components } from '$lib/slices';
 </script>
 
